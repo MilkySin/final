251c2ec67794fbb5dbc1695e949ff3a62d9264c9
ShirinLP <S3983427@rmit.edu.vn>
2023-05-04 13:23:48 +0700

    updated scene

--- src/main/java/com/example/hello2/Controller/Scene5Controller.java
+++ src/main/java/com/example/hello2/Controller/Scene5Controller.java
@@ -1,9 +1,11 @@
 package com.example.hello2.Controller;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,20 +16,35 @@ public class Scene5Controller {
     private Button viewTextFileButton;
 
     @FXML
-    public void viewTextFile() {
-        try {
-            String content = Files.readString(Paths.get("C:/Users/ShirinLP/IdeaProjects/Hello2/new_items.txt"));
+    private ProgressBar progressBar;
+
+    @FXML
+    public void viewTextFile(ActionEvent event) {
+        Task<String> task = new Task<String>() {
+            @Override
+            protected String call() throws Exception {
+                String content = Files.readString(Paths.get("C:/Users/ShirinLP/IdeaProjects/Hello2/new_items.txt"));
+                return content;
+            }
+        };
+
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressBar.setVisible(true);
+
+        task.setOnSucceeded(e -> {
+            String content = task.getValue();
             Alert alert = new Alert(Alert.AlertType.INFORMATION, content);
             alert.setHeaderText("Text File Contents");
             alert.showAndWait();
-        } catch (IOException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Error reading file: " + e.getMessage());
-            alert.showAndWait();
-        }
-    }
+            progressBar.setVisible(false);
+        });
 
-    @FXML
-    public void viewFile(ActionEvent event) {
-        viewTextFile();
+        task.setOnFailed(e -> {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Error reading file: " + task.getException().getMessage());
+            alert.showAndWait();
+            progressBar.setVisible(false);
+        });
+
+        new Thread(task).start();
     }
 }
--- src/main/java/com/example/hello2/Controller/Scene6Controller.java
+++ src/main/java/com/example/hello2/Controller/Scene6Controller.java
@@ -1,4 +1,58 @@
 package com.example.hello2.Controller;
 
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Scene6Controller {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Button viewTextFileButton;
+
+    @FXML
+    public void viewTextFile(ActionEvent event) {
+        Task<String> task = new Task<String>() {
+            @Override
+            protected String call() throws Exception {
+                String content = Files.readString(Paths.get("C:/Users/ShirinLP/IdeaProjects/Hello2/new_items.txt"));
+                return content;
+            }
+        };
+
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressBar.setVisible(true);
+
+        task.setOnSucceeded(e -> {
+            String content = task.getValue();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, content);
+            alert.setHeaderText("Text File Contents");
+            alert.showAndWait();
+            progressBar.setVisible(false);
+        });
+
+        task.setOnFailed(e -> {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Error reading file: " + task.getException().getMessage());
+            alert.showAndWait();
+            progressBar.setVisible(false);
+        });
+
+        new Thread(task).start();
+    }
+
+    public void setLabelText(String text) {
+        label.setText(text);
+    }
 }
