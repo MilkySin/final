15c999aa17aa7668e9f4953f9a2df12435aa0d3e
milkyway <blannkisblank@gmail.com>
2023-05-06 12:39:23 +0700

    added functions

--- src/main/java/com/example/hello2/Controller/DisplayItemsAvailController.java
+++ src/main/java/com/example/hello2/Controller/DisplayItemsAvailController.java
@@ -32,23 +32,52 @@ public class DisplayItemsAvailController {
 //                        System.out.println(line);
 //                }
 //            }
+//                String line;
+//                String id = "";
+//                while ((line = br.readLine()) != null) {
+//                    String[] fields = line.split(":\\s");
+//                    if (fields[0].equals("ID")) {
+//                        id = fields[1];
+//                    } else if (fields[0].equals("Rental Status") && fields[1].equals("Available")) {
+//                        System.out.println(id);
+//                    }
+//                }
             String line;
-            String id = "";
-            boolean available = false;
-            while ((line = br.readLine()) != null) {
-                if (line.contains("Rental Status: Available")) {
-                    available = true;
-                }
-                if (available) {
-                    System.out.println(line);
-                    if (line.contains("ID:")) {
-                        id = line.split(":\\s")[1];
-                        System.out.println("ID: " + id + "\n");
+            String id = "", title = "", rentalType = "", loanType = "", copies = "", rentalFee = "";
+            StringBuilder sb = new StringBuilder();
 
-                    } else if (line.isEmpty()) {
-                        System.out.println("ID: " + id + "\n");
-                        available = false;
-                    }
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(":\\s");
+                switch (fields[0]) {
+                    case "ID":
+                        id = fields[1];
+                        break;
+                    case "Title":
+                        title = fields[1];
+                        break;
+                    case "Rental Type":
+                        rentalType = fields[1];
+                        break;
+                    case "Loan Type":
+                        loanType = fields[1];
+                        break;
+                    case "Copies":
+                        copies = fields[1];
+                        break;
+                    case "Rental Fee (USD)":
+                        rentalFee = fields[1];
+                        break;
+                    case "Rental Status":
+                        if (fields[1].equals("Available")) {
+                            sb.append("ID: ").append(id).append("\n");
+                            sb.append("Title: ").append(title).append("\n");
+                            sb.append("Rental Type: ").append(rentalType).append("\n");
+                            sb.append("Loan Type: ").append(loanType).append("\n");
+                            sb.append("Copies: ").append(copies).append("\n");
+                            sb.append("Rental Fee: ").append(rentalFee).append("\n\n");
+                        }
+
+                        textArea.setText(String.valueOf(sb));
                 }
             }
         }catch(IOException e){
