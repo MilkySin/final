3e799a0f470dd928cb90597df5c49a830cdef462
milkyway <blannkisblank@gmail.com>
2023-05-09 13:26:13 +0700

    I'm smart

--- src/main/java/com/example/hello2/Reader/UserFileReader.java
+++ src/main/java/com/example/hello2/Reader/UserFileReader.java
@@ -32,15 +32,6 @@ public class UserFileReader {
         while((line = bw.readLine()) != null){
             String[] field = line.split(",");
             UserModel user = new UserModel(field[0], field[1],field[2],field[3],field[4],Integer.parseInt(field[5]));
-
-            /*if (field[4].equals("Guest")){
-                GuestAccount guestUser = new GuestAccount(field[6],field[7],field[1],field[0],field[2],Integer.parseInt(field[3]),new ArrayList<>());
-            } else if (field[4].equals("Regular")){
-                RegularAccount regUser = new RegularAccount(field[6],field[7],field[1],field[0],field[2],Integer.parseInt(field[3]),new ArrayList<>());
-            } else if (field[4].equals("VIP")){
-                VIPAccount VIPUser = new VIPAccount(field[6],field[7],field[1],field[0],field[2],Integer.parseInt(field[3]),new ArrayList<>());
-            }*/
-
             userList.add(user);
         }
         return userList;
