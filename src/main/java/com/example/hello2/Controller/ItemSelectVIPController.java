6bf8bca022aee2f96810baf506bd0d4c9c446ee2
milkyway <blannkisblank@gmail.com>
2023-05-12 14:16:24 +0700

    I'm smart

--- src/main/java/com/example/hello2/Controller/ItemSelectVIPController.java
+++ src/main/java/com/example/hello2/Controller/ItemSelectVIPController.java
@@ -2,8 +2,10 @@ package com.example.hello2.Controller;
 
 import com.example.hello2.Model.ItemModel;
 import com.example.hello2.Model.SelectedItems;
+import com.example.hello2.Model.UserModel;
 import com.example.hello2.Reader.ItemsFileReader;
 import com.example.hello2.Reader.SelectedItemsReader;
+import com.example.hello2.Reader.UserFileReader;
 import com.example.hello2.Writer.ItemsFileWriter;
 import com.example.hello2.Writer.SelectedItemsWriter;
 import javafx.concurrent.Task;
@@ -53,6 +55,20 @@ public class ItemSelectVIPController {
     public void setUserID(int userID) {
         this.userID = userID;
     }
+    public void initialize() throws IOException {
+        UserFileReader userFileReader = new UserFileReader();
+        SelectedItemsWriter selectedItemsWriter = new SelectedItemsWriter();
+
+        ArrayList<SelectedItems> con = new SelectedItemsReader().readFileSelectedItems();
+
+        if (con.isEmpty()) {
+            for (UserModel user : userFileReader.readFileUser()) {
+                SelectedItems con3 = new SelectedItems(user.getId());
+                con.add(con3);
+                selectedItemsWriter.SelectedItemsWriteFIle(con);
+            }
+        }
+    }
 
     @FXML
     public void rentItems() throws IOException {
@@ -80,14 +96,15 @@ public class ItemSelectVIPController {
         for (SelectedItems items : selectedItemsReader.readFileSelectedItems()) {
             for (CheckBox check : checkBoxList) {
                 if (Objects.equals(items.getID(), getUserID())) {
-                    for (String i : items.getSelectedItemsList()) {
-                        if (Objects.equals(i, check.getUserData())) {
+                    for (String sd : items.getSelectedItemsList()) {
+                        if (Objects.equals(sd, check.getUserData())) {
                             check.setDisable(true);
                         }
                     }
                 }
             }
         }
+
         // decrement copies value of selected item
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText("Select an item from the list:");
@@ -106,15 +123,25 @@ public class ItemSelectVIPController {
                 }
             }
         }
-
-        // save the selected items to a file
         ArrayList<String> temp = new ArrayList<>();
         for (CheckBox checkBox : checkBoxList) {
             if (checkBox.isSelected()) {
                 temp.add((String) checkBox.getUserData());
-                SelectedItems yes = new SelectedItems(getUserID(), temp);
-                selectedItemsReader.readFileSelectedItems().add(yes);
+            }
+        }
+
+        for (SelectedItems list : selectedItemsReader.getSelectedItemsList()) {
+            if (list.getSelectedItemsList().isEmpty() && Objects.equals(list.getID(), ID)) {
+                list.setSelectedItemsList(temp);
+                System.out.println(list.getSelectedItemsList());
                 selectedItemsWriter.SelectedItemsWriteFIle(selectedItemsReader.getSelectedItemsList());
+                break;
+            }
+            if (!(list.getSelectedItemsList().isEmpty()) && Objects.equals(list.getID(), ID)) {
+                list.getSelectedItemsList().addAll(temp);
+                System.out.println(list.getSelectedItemsList());
+                selectedItemsWriter.SelectedItemsWriteFIle(selectedItemsReader.getSelectedItemsList());
+                break;
             }
         }
     }
@@ -142,11 +169,12 @@ public class ItemSelectVIPController {
 
         ArrayList<ItemModel> itemModelArrayList = itemsFileReader.readFileItems();
 
-        for(SelectedItems temp : selectedItemsReader.readFileSelectedItems()) {
+        for (SelectedItems temp : selectedItemsReader.readFileSelectedItems()) {
             for (ItemModel items : itemModelArrayList) {
-                if (temp.getSelectedItemsList().contains(items.getID())) {
+                if (temp.getSelectedItemsList().contains(items.getID()) && Objects.equals(temp.getID(), ID)) {
                     CheckBox checkBox = new CheckBox(items.toString());
                     checkBox.setUserData(items.getID());
+
                     HBox itemBox = new HBox();
                     if (items.getCopies() == 0) {
                         checkBox.setDisable(false);
@@ -173,12 +201,16 @@ public class ItemSelectVIPController {
                 }
             }
         }
-
         ArrayList<String> temp = new ArrayList<>();
-        for(CheckBox checkBox : checkBoxList){
-            if(checkBox.isSelected()) {
-                temp.remove((String) checkBox.getUserData());
-                SelectedItems yes = new SelectedItems(getUserID(), temp);
+        for (CheckBox checkBox : checkBoxList) {
+            if (checkBox.isSelected()) {
+                temp.add((String) checkBox.getUserData());
+            }
+        }
+
+        for (SelectedItems list : selectedItemsReader.getSelectedItemsList()) {
+            if (Objects.equals(list.getID(), ID)) {
+                list.getSelectedItemsList().removeAll(temp);
                 selectedItemsWriter.SelectedItemsWriteFIle(selectedItemsReader.getSelectedItemsList());
             }
         }
